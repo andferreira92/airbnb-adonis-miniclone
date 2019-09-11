f5cb6b550c56dba49a896c61f3ded5616ff8e5ad
Andson ferreira <andferreira92@outlook.com>
2019-09-11 17:28:56 -0300

    CRUD

--- app/Models/User.js
+++ app/Models/User.js
@@ -18,7 +18,8 @@ class User extends Model {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
-    })
+    });
+
   }
 
   /**
@@ -33,6 +34,10 @@ class User extends Model {
    */
   tokens () {
     return this.hasMany('App/Models/Token')
+  }
+
+  properties () {
+    return this.hasMany('App/Models/Property')
   }
 }
 
